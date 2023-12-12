26724a18f047535fe27d439354d22d0b798c84cf
Asai Toshiya <to.asai.60@gmail.com>
2023-12-12 21:49:49 +0900

    fetchResourceEvents() を index.html に移動させる

--- src/convertNip21sToResource.js
+++ src/convertNip21sToResource.js
@@ -22,29 +22,3 @@ const convertNip21sToResource = async (document) => {
         (img.src = `data:${typeTag[1]};base64,${event.content}`);
   });
 };
-
-// { [nip21: `nostr:${string}`]: Event<number> }[] を返す
-const fetchResourceEvents = async (document) => {
-  const nip21s = [
-    ...document.querySelectorAll(
-      "script[src^='nostr:'], link[href^='nostr:'], img[src^='nostr:']"
-    ),
-  ].map((el) => el.src || el.href);
-
-  const pool = new window.NostrTools.SimplePool();
-  const filters = nip21s.map(createFilter);
-  const events = await pool.list(relays, filters);
-  await pool.close(relays);
-
-  return nip21s.reduce((acc, obj) => {
-    const { type, data } = window.NostrTools.nip21.parse(obj).decoded;
-    const event = events.find((event) =>
-      type === "nevent"
-        ? event.id == data.id
-        : event.kind == data.kind &&
-          event.pubkey == data.pubkey &&
-          findTag(event, "d")[1] == data.identifier
-    );
-    return { ...acc, [obj]: event };
-  }, {});
-};
