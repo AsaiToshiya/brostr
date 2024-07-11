58f91f617b4fdff88410eab7bdb66f462b8825f6
Asai Toshiya <to.asai.60@gmail.com>
2024-07-12 01:52:29 +0900

    アイコンのパスを修正する

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const createWindow = () => {
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
-    icon: '/icons/icon.png'
+    icon: path.join(__dirname, '../icons/icon.png')
   });
 
   // and load the index.html of the app.
