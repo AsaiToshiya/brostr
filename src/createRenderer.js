4cbe1e511d4b83c1a7c4c3bcae8d6e4c42a391ca
Asai Toshiya <to.asai.60@gmail.com>
2024-09-19 23:31:02 +0900

    レンダリングするための関数を新しいファイルに移動させる

---
+++ src/createRenderer.js
@@ -0,0 +1,132 @@
+const createRenderer = (event) =>
+  event.kind == 5392 || event.kind == 35392
+    ? renderHtmlContent
+    : event.kind == 30023
+    ? renderLongFormContent
+    : event.kind == 64
+    ? renderChess
+    : ((event.kind == 1063 || event.kind == 1064) &&
+      findTag(event, "m")[1].startsWith("image/"))
+    ? renderImage
+    : null;
+
+const renderChess = async (event) => {
+  iframe.srcdoc = `<!DOCTYPE html>
+<html>
+  <head>
+    <script src="../pgnviewer/jsPgnViewer.js"><\/script>
+  </head>
+  <body>
+    <div
+      id="fen1_board"
+      style="max-width: 940px; margin-left: auto; margin-right: auto"
+    ></div>
+    <div id="fen1" style="visibility: hidden; display: none">
+      ${event.content}
+    </div>
+    <script>
+      var brd = new Board("fen1", {
+        imagePrefix: "../pgnviewer/img/zurich/",
+      });
+      brd.init();
+    <\/script>
+  </body>
+</html>`;
+};
+
+const renderHtmlContent = async (event) => {
+  const convertUrlsToProxyUrl = (document, proxyUrl) => {
+    [...document.querySelectorAll("script, link, img")]
+      .filter((el) => el.getAttribute("src") || el.getAttribute("href"))
+      .forEach((el) => {
+        const originalValue =
+          el.getAttribute("src") || el.getAttribute("href");
+        const newUrl = new URL(originalValue, proxyUrl);
+        el.src && (el.src = newUrl);
+        el.href && (el.href = newUrl);
+      });
+  };
+
+  const normalizeHtml = async (event) => {
+    const proxyTag = findTag(event, "proxy");
+    const isWebProxy = proxyTag?.[2] == "web";
+
+    virtualDom.srcdoc = isWebProxy
+      ? await (await fetch(proxyTag[1])).text()
+      : event.content;
+    return new Promise(
+      (resolve) =>
+        (virtualDom.onload = async () => {
+          isWebProxy
+            ? convertUrlsToProxyUrl(
+                virtualDom.contentWindow.document,
+                proxyTag[1]
+              )
+            : await convertNip21sToResource(
+                virtualDom.contentWindow.document
+              );
+
+          resolve(
+            unescapeXML(
+              new XMLSerializer().serializeToString(
+                virtualDom.contentWindow.document
+              )
+            )
+          );
+        })
+    );
+  };
+
+  iframe.srcdoc = await normalizeHtml(event);
+};
+
+const renderImage = async (event) => {
+  const mimeType = findTag(event, "m")[1];
+  const url =
+    event.kind == 1063
+      ? findTag(event, "url")[1]
+      : `data:${mimeType};base64,${event.content}`;
+  iframe.srcdoc = `<!DOCTYPE html>
+<html class="h-full m-0 p-0 w-full">
+  <head>
+    <script src="https://cdn.tailwindcss.com"><\/script>
+  <\/head>
+  <body class="h-full m-0 p-0 w-full bg-black">
+    <img src="${url}" class="w-full h-full object-scale-down" \/>
+  <\/body>
+<\/html>`;
+};
+
+const renderLongFormContent = async (event) => {
+  const loadOracolo = async (event) => {
+    const contentWindow = virtualDom.contentWindow;
+    contentWindow.location.replace("../oracolo/dist/index.html");
+    return new Promise(
+      (resolve) =>
+        (virtualDom.onload = () => {
+          const document = contentWindow.document;
+          const author = document.querySelector("meta[name='author']");
+          const relays = document.querySelector("meta[name='relays']");
+          const comments = document.querySelector(
+            "meta[name='comments']"
+          );
+          author.setAttribute(
+            "value",
+            window.NostrTools.nip19.npubEncode(event.pubkey)
+          );
+          relays.setAttribute("value", defaultRelays.toString());
+          comments.setAttribute("value", "");
+
+          resolve(
+            unescapeXML(new XMLSerializer().serializeToString(document))
+          );
+        })
+    );
+  };
+
+  iframe.contentWindow.location.replace("about:blank");
+  iframe.srcdoc = await loadOracolo(event);
+  iframe.contentWindow.location.replace(`about:srcdoc#${event.id}`);
+};
+
+module.exports = createRenderer;
